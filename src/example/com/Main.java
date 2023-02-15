49cf56f664a90bb899a7a43230a7d5d564216629
yanivcohen223 <yanivcohen223@gmail.com>
2023-02-15 14:36:31 +0200

    upload

--- src/example/com/Main.java
+++ src/example/com/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        Car car1 = new Car("A3", "BMW", "black", 85);
+        Car car2 = new Car("fluence", "renault", "cream");
+        Car car3 = new Car("Sonic", "Chevy","white",50000,120.5f);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+
     }
 }
